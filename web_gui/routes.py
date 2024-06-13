e79ede48de30d543b161acfac81261a4b70cdd40
Zuhaitz Beloki Leitza <z.beloki@elhuyar.eus>
2024-06-13 22:00:55 +0200

    Filters added to other pages

--- web_gui/routes.py
+++ web_gui/routes.py
@@ -14,7 +14,8 @@ def home():
 def request_queue():
     mongo = current_app.config['MONGO']
     data = mongo.db.request_queue.find({"status": "pending"}).sort("payload.next_update_at", 1)
-    data = utils.filter_by_pages(data, request)
+    get_page_f = lambda e: e['payload']['page']
+    data = utils.filter_by_pages(data, get_page_f, request)
     available_page_names = mongo.db.request_queue.distinct("payload.page.page_name")
     return render_template(
         'pending_requests.html',
@@ -27,22 +28,58 @@ def request_queue():
 def completed_requests():
     mongo = current_app.config['MONGO']
     data = mongo.db.request_queue.find({"status": "completed"}).sort("payload.last_updated_at", -1)
-    return render_template('completed_requests.html', data=data)
+    get_page_f = lambda e: e['payload']['page']
+    data = utils.filter_by_pages(data, get_page_f, request)
+    available_page_names = mongo.db.request_queue.distinct("payload.page.page_name")
+    return render_template(
+        'completed_requests.html',
+        data=data,
+        page_names=available_page_names,
+        query_params=request.args,        
+    )
 
 @main_bp.route('/failed')
 def failed_requests():
     mongo = current_app.config['MONGO']
     data = mongo.db.request_queue.find({"status": "failed"}).sort("payload.last_updated_at", -1)
-    return render_template('failed_requests.html', data=data)
+    get_page_f = lambda e: e['payload']['page']
+    data = utils.filter_by_pages(data, get_page_f, request)
+    available_page_names = mongo.db.request_queue.distinct("payload.page.page_name")
+    return render_template(
+        'failed_requests.html',
+        data=data,
+        page_names=available_page_names,
+        query_params=request.args,        
+    )
 
 @main_bp.route('/archived')
 def archived_pages():
     mongo = current_app.config['MONGO']
     data = mongo.db.archived_pages.find().sort("payload.archived_at", -1)
-    return render_template('archived_pages.html', data=data)
+    get_page_f = lambda e: e
+    data = utils.filter_by_pages(data, get_page_f, request)
+    available_page_names = mongo.db.request_queue.distinct("payload.page.page_name")
+    return render_template(
+        'archived_pages.html',
+        data=data,
+        page_names=available_page_names,
+        query_params=request.args,        
+    )
 
 @main_bp.route('/data')
 def data_items():
     mongo = current_app.config['MONGO']
     data = mongo.db.item_store.find().sort("parsed_at", -1)
-    return render_template('crawled_data.html', data=data)
+    get_page_f = lambda e: e['page']
+    get_item_f = lambda e: e['item']
+    data = utils.filter_by_pages(data, get_page_f, request)
+    data = utils.filter_by_items(data, get_item_f, request)
+    available_item_types = mongo.db.item_store.distinct("item._type")
+    available_page_names = mongo.db.item_store.distinct("page.page_name")
+    return render_template(
+        'crawled_data.html',
+        data=data,
+        item_types=available_item_types,
+        page_names=available_page_names,
+        query_params=request.args,        
+    )
--- web_gui/utils.py
+++ web_gui/utils.py
@@ -1,23 +1,44 @@
 from flask import current_app
 
-def filter_by_pages(request_objs, request):
+def filter_by_pages(data, get_page_f, request):
     page_name = request.args.get('page_name', '')
     page_key_pattern = request.args.get('page_key', '')
     if page_name.strip() != "":
-        request_objs = filter_requests_by_page_name(request_objs, page_name)
+        data = filter_by_page_name(data, get_page_f, page_name)
     if page_key_pattern.strip() != "":
-        request_objs = filter_requests_by_search(request_objs, page_key_pattern)
-    return request_objs
+        data = filter_by_search_key(data, get_page_f, page_key_pattern)
+    return data
 
-def filter_requests_by_page_name(request_objs, page_name):
-    return [ req for req in request_objs if req['payload']['page']['page_name'] == page_name ]
+def filter_by_page_name(data, get_page_f, page_name):
+    return [ e for e in data if get_page_f(e)['page_name'] == page_name ]
 
-def filter_requests_by_search(request_objs, pattern):
+def filter_by_search_key(data, get_page_f, pattern):
     format_key = current_app.jinja_env.filters['format_key']
-    result_requests = []
-    for request_obj in request_objs:
-        request_str = format_key(request_obj['payload']['page']['key']).lower()
-        if all([ token in request_str for token in pattern.lower().split() ]):
-            result_requests.append(request_obj)
-    return result_requests
+    result = []
+    for e in data:
+        e_str = format_key(get_page_f(e)['key']).lower()
+        if all([ token in e_str for token in pattern.lower().split() ]):
+            result.append(e)
+    return result
+
+def filter_by_items(data, get_item_f, request):
+    item_type = request.args.get('item_type', '')
+    item_pattern = request.args.get('item_pattern', '')
+    if item_type.strip() != "":
+        data = filter_by_item_type(data, get_item_f, item_type)
+    if item_pattern.strip() != "":
+        data = filter_by_search_data(data, get_item_f, item_pattern)
+    return data    
+
+def filter_by_item_type(data, get_item_f, item_type):
+    return [ e for e in data if get_item_f(e)['_type'] == item_type ]
+
+def filter_by_search_data(data, get_item_f, pattern):
+    format_item = current_app.jinja_env.filters['format_item']
+    result = []
+    for e in data:
+        e_str = format_item(get_item_f(e)).lower()
+        if all([ token in e_str for token in pattern.lower().split() ]):
+            result.append(e)
+    return result
 
